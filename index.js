17e3b6f624cc0c33e836660a0c08530acf49eddf
ebrammamdoh <ebrammamdoh5@gmail.com>
2018-09-03 22:08:55 +0200

    final code refactoring

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-
-
+const winston = require('winston');
 
 require('./startup/routes')(app);
 require('./startup/db')();
@@ -11,4 +10,4 @@ require('./startup/logging')();
 require('./startup/config')();
 
 const port = process.env.PORT || 3000;
-app.listen(port , () => {console.log('server started')});
+app.listen(port , () => {winston.info(`server started at port: ${port}`)});
